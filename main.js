42eea18e32f204d54e58081c045ce1960e6ece46
jamaluddin <kuramagomedovzaur8@gmail.com>
2026-08-11 15:55:15 +0300

    fixed the errors.

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
- const productCard = document.querySelector ('.product-card');
-
- const changeCardColorButton = document.querySelector ('.change-card-color-button');
-
+ const productCard = document.querySelector('.product-card');
+ const changeCardColorButton = document.querySelector('.change-card-color-button');
  const greenColorHash = '#00ff00';
  
  changeCardColorButton.addEventListener('click', () => {
@@ -9,64 +7,49 @@
  })
 
 
- const productCards = document.querySelectorAll ('.product-card');
- 
- const changeColorButtonCard = document.querySelector ('.change-color-button-card');
- 
+ const productCards = document.querySelectorAll('.product-card');
+ const changeColorButtonCard = document.querySelector('.change-color-button-card');
  const redColorHash = 'red';
-
- changeColorButtonCard.addEventListener ('click', () => {
- productCards.forEach ((cards) => cards.style.backgroundColor = redColorHash)
+ changeColorButtonCard.addEventListener('click', () => {
+ productCards.forEach((cards) => cards.style.backgroundColor = redColorHash)
  }) 
 
 
-
- const openGoogl = document.querySelector ('#open-google')
- 
- openGoogl.addEventListener ('click' , openGoogle)
-
-
+ const openGoogl = document.querySelector('#open-google')
+ openGoogl.addEventListener('click' , openGoogle)
  function openGoogle() {
- 
-    const answer = confirm ('Вы действительно хотите открыть Google');
- 
-   if (answer === true) {
-    window.open('https://google.com')
+ const answer = confirm('Вы действительно хотите открыть Google');
+ if (answer === true) {
+ window.open('https://google.com')
  } else {
-    return;
+ return;
  }
 }
 
-const outputConsoleButton = document.querySelector ('#output-console-log');
-
+const outputConsoleButton = document.querySelector('#output-console-log');
 outputConsoleButton.addEventListener('click' , () => outputConsoleLog ('ДЗ №4'));
-
-function outputConsoleLog (message) {
-    alert(message)
-    console.log(message)
+function outputConsoleLog(message) {
+alert(message)
+console.log(message)
 }
 
 const carTitle = document.querySelector('.card-title')
-
 carTitle.addEventListener('mouseover' , () => {
-
-   console.log(carTitle.textContent);
+console.log(carTitle.textContent);
 });
 
 const myButton = document.getElementById('color-btn');
-
 const myText = document.getElementById('color-btn');
-
 let isRed = false;
 myButton.addEventListener('click' , function () {
-  if(isRed) {
-   myText.style.backgroundColor = 'blue';
-  myText.textContent = 'цвет текста синий';
-  isRed = false;
+if(isRed) {
+myText.style.backgroundColor = 'blue';
+myText.textContent = 'цвет текста синий';
+isRed = false;
 } else {
-   myText.style.backgroundColor = 'red';
-   myText.textContent = 'цвет текста красный';
+myText.style.backgroundColor = 'red';
+myText.textContent = 'цвет текста красный';
 
-   isRed = true;
+isRed = true;
 }
 })
