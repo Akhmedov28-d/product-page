aaa494c172609a842c2cdde84dfad1bf2c6c0bfe
jamaluddin <kuramagomedovzaur8@gmail.com>
2026-09-07 15:36:09 +0300

    fixed errors

--- homework-8.js
+++ homework-8.js
@@ -1,34 +1,32 @@
-
 const person = {
-name: "Дамалудин",
-surname: "Ахмедов",
-mail: "kuramagomedovzaur8@gmail.com",
-job: "Mid-level Разработчик",
-jobTitle: "Фронтент Разработчик на Базе Ангуляр",
-age: 28,
-country: "Киргизия",
-city: "Токмок",
-relationshipStatus: "Разведен",
-}
+  name: "Джамалудин",
+  surname: "Ахмедов",
+  mail: "kuramagomedovzaur8@gmail.com",
+  job: "Mid-level Разработчик",
+  jobTitle: "Фронтент Разработчик на Базе Ангуляр",
+  age: 28,
+  country: "Киргизия",
+  city: "Токмок",
+  relationshipStatus: "Разведен",
+};
 
 const techPassport = {
-make: "Mercedes-Benz_W140",
-model: "S_600",
-year: 1998,
-color: "black",
-transmission: "manual_transmission"
-
-}
+  make: "Mercedes-Benz_W140",
+  model: "S_600",
+  year: 1998,
+  color: "black",
+  transmission: "manual_transmission",
+};
 
 person.car_owner = "Джамалудин";
 
 console.log(person);
 
 function addMaxSpeed(car) {
-    if ('maxSpeed' in car) {
-        return;
-    }
- car.maxSpeed = 220;
+  if ("maxSpeed" in car) {
+    return;
+  }
+  car.maxSpeed = 220;
 }
 
 addMaxSpeed(techPassport);
@@ -36,77 +34,83 @@ addMaxSpeed(techPassport);
 console.log(techPassport);
 
 function characteristic(vipClass, beautifulColor) {
-    console.log(vipClass[beautifulColor]);
+  return vipClass[beautifulColor]
 }
 
-characteristic(techPassport, "color");
+(characteristic(techPassport, "color"))
 
-const healthyFoods = ['Жирная рыба', 'Ягоды', 'Авокадо', 'Грецкие орехи', 'Шпинат'];
+const healthyFoods = [
+  "Жирная рыба",
+  "Ягоды",
+  "Авокадо",
+  "Грецкие орехи",
+  "Шпинат",
+];
 console.log(healthyFoods, [3]);
 
 const books = [
- {
-  name: "Этикет ищущего знание",
-  author: "Шейх Мухаммад Ибн Солих Аль-Усеймин",
-  year: 2022,
-  color: "white",
-  genre: "religion",
- },
+  {
+    name: "Этикет ищущего знание",
+    author: "Шейх Мухаммад Ибн Солих Аль-Усеймин",
+    year: 2022,
+    color: "white",
+    genre: "religion",
+  },
 
- {
-  name: "Малое Завешание",
-  author: "Ибн Таймийя",
-  year: 2020,
-  color: "green",
-  genre: "religion",
- },
+  {
+    name: "Малое Завешание",
+    author: "Ибн Таймийя",
+    year: 2020,
+    color: "green",
+    genre: "religion",
+  },
 
- {
-  name: "40 хадисов",
-  author: "Шейх Солих Фузан",
-  year: 2021,
-  color: "black",
-  genre: "religion",
- }
+  {
+    name: "40 хадисов",
+    author: "Шейх Солих Фузан",
+    year: 2021,
+    color: "black",
+    genre: "religion",
+  },
 ];
 
 const book = {
-   name: "Разьяснение трех основ",
+  name: "Разьяснение трех основ",
   author: "Мухаммад Ат-Тамими",
   year: 2020,
   color: "green",
   genre: "religion",
- };
+};
 
 books.push(book);
 
 const classicBooks = [
- {
-  name: "ОТЕЦ",
-  author: "Абдулманап Нурмагомедов",
-  year: 2020,
-  color: "white",
-  genre: "sport",
- },
+  {
+    name: "ОТЕЦ",
+    author: "Абдулманап Нурмагомедов",
+    year: 2020,
+    color: "white",
+    genre: "sport",
+  },
 
- {
-  name: "Тренажер для развития мозга",
-  author: "Антон Могучий",
-  year: 2018,
-  color: "green",
-  genre: "religion",
- },
+  {
+    name: "Тренажер для развития мозга",
+    author: "Антон Могучий",
+    year: 2018,
+    color: "green",
+    genre: "religion",
+  },
 
- {
-  name: "Зов Предков",
-  author: "Джек Лондон",
-  year: 2021,
-  color: "black",
-  genre: "classic",
- }
+  {
+    name: "Зов Предков",
+    author: "Джек Лондон",
+    year: 2021,
+    color: "black",
+    genre: "classic",
+  },
 ];
 
 const allBooks = [...books, ...classicBooks];
 
 const genre = classicBooks;
-const comparison = classicBooks.map(book => book.genre === "sport");
+const comparison = classicBooks.map((book) => book.genre === "sport");
